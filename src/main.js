af2a397aad5cccd09dcf26668f2a7c0c09bf4ce9
CristianLenovoU22 <cristianoso19@gmail.com>
2023-09-24 16:50:30 -0500

    active states: primary input border

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log(e);
+const form = document.getElementById('form');
+const dayInput = document.getElementById('year');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault;
+    if (validateForm){
+        showAge();
+    } else {
+        showErrors();
+    }
+});
