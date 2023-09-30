e0c7d098b791071faa0bb8a1ba6ad09275141544
Cristian Sacta <cristianoso19@gmail.com>
2023-09-30 17:09:27 -0500

    Improve html inputs lenght and make a validation for date formats: yyyy/m/d

--- src/main.js
+++ src/main.js
@@ -25,22 +25,22 @@ function validateForm(){
     const allConditionsMet = conditions.every(condition => condition === true);
 
     if (allConditionsMet){
-        reviewDate();
-        return true;
-    } else {
-        return false;
+        return reviewDate();
     }
 }
 
 function reviewDate(){
-    const inputDate = yearInput.value+'-'+monthInput.value+'-'+dayInput.value;
-    if (validateDate(inputDate)){
-        if (!validateFuture(inputDate)){
+
+    if (validateDate()){
+        if (validateFuture()){
+            return true;
+        } else {
             setErrorOnFormat("Must be in the past");
         }
     } else {
         setErrorOnFormat("Invalid date");
     }
+    return false;
 }
 
 function showAge(age){
@@ -98,8 +98,10 @@ function calculateAge(birthdayStr) {
     };
   }
 
-const validateFuture =  (date)=>{
-    const inputDate = new Date(date);
+const validateFuture =  ()=>{
+    const dateStr = yearInput.value+'-'+monthInput.value+'-'+dayInput.value;
+    const dateStrFormat = formatDateString(dateStr)
+    const inputDate = new Date(dateStrFormat);
     const actualDate = new Date();
 
     return inputDate<=actualDate;
@@ -112,10 +114,11 @@ const setErrorOnFormat = (message) => {
     setError(yearInput,message);
 }
 
-const validateDate = (date) => {
+const validateDate = () => {
+    const inputDate = yearInput.value+'-'+monthInput.value+'-'+dayInput.value;
     // Object.prototype.toString.call(object) is a common technique to get the type of an object in JavaScript in a more precise and reliable way than other approaches, as it works for a variety of object types.
-    const dateStr = date;
-    date = new Date(date);
+    const dateStr = formatDateString(inputDate);
+    const date = new Date(dateStr);
 
     if (Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)) {
         return date.toISOString().slice(0, 10) === dateStr;
@@ -123,6 +126,18 @@ const validateDate = (date) => {
       
     return false;
 }
+function formatDateString(input) {
+    // Use regex to find single-digit patterns in the format yyyy/m/d
+    const regex = /(\d{4})\-(\d{1,2})\-(\d{1,2})/;
+    
+    // Use replace with a replacement function to format the date
+    const formattedDate = input.replace(regex, function(match, year, month, day) {
+      // Add leading zeros to single-digit months and days
+      return year + '-' + (month.length === 1 ? '0' + month : month) + '-' + (day.length === 1 ? '0' + day : day);
+    });
+    console.log(formattedDate);
+    return formattedDate;
+  }
 
 const actualDate = () => {
     const actualDate = new Date.now();
@@ -174,12 +189,12 @@ function reviewYear(){
 }
 
 function validateDay (value){
-    let regexPattern =/^(0[1-9]|[1-2][0-9]|3[0-1])$/;
+    let regexPattern =/^(0?[1-9]|[12][0-9]|3[01])$/;
     return regexPattern.test(value); 
 }
 
 function validateMonth (value){
-    let regexPattern =/0[1-9]|1[0-2]/;
+    let regexPattern =/^(0?[1-9]|1[0-2])$/;
     return regexPattern.test(value); 
 }
 
